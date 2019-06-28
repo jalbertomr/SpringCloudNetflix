1285aad513b8dd60ecabf56e960f99fb16915704
Jose Alberto Martinez <jalbertomr@yahoo.com>
2019-06-27 20:09:49 -0500

    Add Eureka Server,change Producer para registrarse en Eureka

--- employee-producer/src/main/java/com/bext/SpringBootHolaMundoApplication.java
+++ employee-producer/src/main/java/com/bext/SpringBootHolaMundoApplication.java
@@ -2,8 +2,10 @@ package com.bext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringBootHolaMundoApplication {
 
 	public static void main(String[] args){
