15554ee6aadcfbb48e5e7591da46b43abf342105
Jose Alberto Martinez <jalbertomr@yahoo.com>
2019-07-02 20:32:42 -0500

    add Hystrix fallback circuit breacker al Productor

--- employee-producer/src/main/java/com/bext/SpringBootProducerApplication.java
+++ employee-producer/src/main/java/com/bext/SpringBootProducerApplication.java
@@ -2,12 +2,14 @@ package com.bext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 import com.bext.model.Info;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 public class SpringBootProducerApplication {
 
--- employee-producer/src/main/java/com/bext/controllers/TestController.java
+++ employee-producer/src/main/java/com/bext/controllers/TestController.java
@@ -6,17 +6,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bext.model.Employee;
 import com.bext.model.Info;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class TestController {
 
 	@RequestMapping(value = "/empleado", method = RequestMethod.GET)
+	@HystrixCommand(fallbackMethod = "getDataFallBack")
 	public Employee firstPage() {
 
 		Employee emp = new Employee();
 		emp.setName("emp1");
 		emp.setDesignation("manager");
 		emp.setEmpId("1");
+		emp.setSalary(3000);
+
+		return emp;
+	}
+	
+	public Employee getDataFallBack() {
+		Employee emp = new Employee();
+		emp.setName("fallback-emp1");
+		emp.setDesignation("fallback-manager");
+		emp.setEmpId("fallback-1");
 		emp.setSalary(3000);
 
 		return emp;
